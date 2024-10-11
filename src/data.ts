bf8700d8f1084a311b432c4567028534a0ed546a
JonashGana <jonashtiglao@gmail.com>
2024-10-11 22:39:57 +0800

    import png and webp

--- src/data.ts
+++ src/data.ts
@@ -46,7 +46,7 @@ export const data = {
       },
       replies: [
         {
-          id: 2.1,
+          id: "2.1",
           content:
             "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
           createdAt: "1 week ago",
@@ -61,7 +61,7 @@ export const data = {
           },
         },
         {
-          id: 2.2,
+          id: "2.2",
           content:
             "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
           createdAt: "2 days ago",
