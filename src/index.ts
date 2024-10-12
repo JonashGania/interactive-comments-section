8233d8ae59b820c8aab7e7eb5887daaa1703fddd
JonashGana <jonashtiglao@gmail.com>
2024-10-12 22:26:01 +0800

    eslint fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import "./style/style.css";
-import { commentSubmission } from "./components/eventHandlers";
-import { Comments } from "./components/classes";
+import './style/style.css';
+import { commentSubmission } from './utils/eventHandlers';
+import { Comments } from './components/classes';
 
 const initializeLoad = (): void => {
   Comments.initializeComments();
