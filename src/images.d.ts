d60aa9832667dd5be803704c529f66a8ffdedcff
JonashGana <jonashtiglao@gmail.com>
2024-10-12 22:25:47 +0800

    eslint fix

--- src/images.d.ts
+++ src/images.d.ts
@@ -1,9 +1,9 @@
-declare module "*.png" {
+declare module '*.png' {
   const value: string;
   export default value;
 }
 
-declare module "*.webp" {
+declare module '*.webp' {
   const value: string;
   export default value;
 }
