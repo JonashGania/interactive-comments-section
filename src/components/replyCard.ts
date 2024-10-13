41826fd1b23ad3227a50a388f95820d715a29fe8
JonashGana <jonashtiglao@gmail.com>
2024-10-13 19:18:13 +0800

    refactor code

--- src/components/replyCard.ts
+++ src/components/replyCard.ts
@@ -10,38 +10,34 @@ import {
   handleReplyButton,
   replySubmission,
 } from '../utils/eventHandlers';
-import { sortedRepliesByScore, getDateCreated } from '../utils/utils';
+import { getDateCreated } from '../utils/utils';
 
-const ReplyCard = (replies: Replies[], commentId: number, replyLength: number) => {
-  const replyLists = <HTMLUListElement>document.createElement('ul');
-  replyLists.classList.add('reply-lists');
+const ReplyCard = (replies: Replies, commentId: number, replyLength: number) => {
+  const { id, content, createdAt, score, user, replyingTo } = replies;
 
-  const sortedReplies = sortedRepliesByScore(replies);
-
-  sortedReplies.forEach((reply) => {
-    const replyElement = <HTMLLIElement>document.createElement('li');
-    replyElement.classList.add('reply-card');
-    replyElement.setAttribute('reply-index', `${reply.id}`);
-    replyElement.innerHTML = `
-        <div class="user-comment-container">
-          <div class="vote-container">
-            <div class="vote-wrapper">
-              <button class="upvote-btn">
-                  <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg" class="plus-icon">
-                      <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/>
-                  </svg>
-              </button>
-              <span class="vote-score">${reply.score}</span>
-              <button class="downvote-btn">
-                  <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg" class="minus-icon">
-                      <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/>
-                  </svg>
-              </button>
-            </div>
+  const replyElement = <HTMLLIElement>document.createElement('li');
+  replyElement.classList.add('reply-card');
+  replyElement.setAttribute('reply-index', `${id}`);
+  replyElement.innerHTML = `
+    <div class="user-comment-container">
+           <div class="vote-container">
+             <div class="vote-wrapper">
+               <button class="upvote-btn">
+                   <svg width="11" height="11" xmlns="http:www.w3.org/2000/svg" class="plus-icon">
+                       <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/>
+                   </svg>
+               </button>
+               <span class="vote-score">${score}</span>
+               <button class="downvote-btn">
+                   <svg width="11" height="3" xmlns="http:www.w3.org/2000/svg" class="minus-icon">
+                       <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/>
+                   </svg>
+               </button>
+             </div>
             
-            ${
-              reply.user.username === data.currentUser.username
-                ? `
+               ${
+                 user.username === data.currentUser.username
+                   ? `
               <div class="edit-delete-container">
                   <button class="delete-btn">
                       <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg" class="delete-icon">
@@ -57,7 +53,7 @@ const ReplyCard = (replies: Replies[], commentId: number, replyLength: number) =
                   </button>
               </div>
             `
-                : `
+                   : `
               <button class="reply-button">
                 <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg" class="reply-icon">
                     <path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/>
@@ -65,19 +61,19 @@ const ReplyCard = (replies: Replies[], commentId: number, replyLength: number) =
                 <span class="reply-span">Reply</span>
               </button>
             `
-            }
+               }
           </div>
 
             <div class="comment-content-container">
                 <div class="content-head">
                     ${
-                      reply.user.username === data.currentUser.username
+                      user.username === data.currentUser.username
                         ? `
                         <div class="comment-head-wrapper">
                             <div class="user-avatar-container"></div>
-                            <h3 class="comment-username">${reply.user.username}</h3>
+                            <h3 class="comment-username">${user.username}</h3>
                             <span class="you-span">you</span>
-                            <span class="comment-date-created">${getDateCreated(reply.createdAt)}</span>
+                            <span class="comment-date-created">${getDateCreated(createdAt)}</span>
                         </div>
                         <div class="edit-delete-container">
                             <button class="delete-btn">
@@ -97,8 +93,8 @@ const ReplyCard = (replies: Replies[], commentId: number, replyLength: number) =
                         : `
                         <div class="comment-head-wrapper">
                             <div class="user-avatar-container"></div>
-                            <h3 class="reply-username">${reply.user.username}</h3>
-                            <span class="reply-date-created">${getDateCreated(reply.createdAt)}</span>
+                            <h3 class="reply-username">${user.username}</h3>
+                            <span class="reply-date-created">${getDateCreated(createdAt)}</span>
                         </div>
                         <button class="reply-button">
                             <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg" class="reply-icon">
@@ -110,7 +106,7 @@ const ReplyCard = (replies: Replies[], commentId: number, replyLength: number) =
                     } 
                 </div>
                 <div>
-                    <p class="user-reply-content"><span class="mention-user">@${reply.replyingTo}</span> ${reply.content}</p>
+                    <p class="user-reply-content"><span class="mention-user">@${replyingTo}</span> ${content}</p>
                     <div class="update-content-container close">
                       <textarea id="update-reply-content" class="update-reply-content" cols="10" rows="5"></textarea>
                       <button class="update-button">Update</button>
@@ -129,75 +125,59 @@ const ReplyCard = (replies: Replies[], commentId: number, replyLength: number) =
             <button class="submit-reply-btn">Reply</button>
             </div>
         </form>
-    `;
+  `;
 
-    const userAvatarContainer = <HTMLDivElement>replyElement.querySelector('.user-avatar-container');
+  const userAvatar = UserAvatar(user.image.png, user.image.webp);
 
-    const userAvatar = UserAvatar(reply.user.image.png, reply.user.image.webp);
+  const userAvatarContainer = <HTMLDivElement>replyElement.querySelector('.user-avatar-container');
+  const deleteBtn = replyElement.querySelectorAll('.delete-btn') as NodeListOf<HTMLButtonElement>;
+  const editBtn = replyElement.querySelectorAll('.edit-btn') as NodeListOf<HTMLButtonElement>;
+  const updateBtn = <HTMLButtonElement>replyElement.querySelector('.update-button');
+  const replyContent = <HTMLParagraphElement>replyElement.querySelector('.user-reply-content');
+  const addReply = <HTMLTextAreaElement>replyElement.querySelector('.add-reply');
+  const replyForm = <HTMLFormElement>replyElement.querySelector('.reply-form');
+  const upvoteBtn = <HTMLButtonElement>replyElement.querySelector('.upvote-btn');
+  const downvoteBtn = <HTMLButtonElement>replyElement.querySelector('.downvote-btn');
+  const voteScoreSpan = <HTMLSpanElement>replyElement.querySelector('.vote-score');
+  const updateContentContainer = <HTMLDivElement>replyElement.querySelector('.update-content-container');
+  const replyBtn = replyElement.querySelectorAll('.reply-button') as NodeListOf<HTMLButtonElement>;
 
-    const deleteBtn = replyElement.querySelectorAll('.delete-btn') as NodeListOf<HTMLButtonElement>;
-    const overlay = <HTMLDivElement>document.querySelector('.overlay');
-    const modalContainer = <HTMLDivElement>document.querySelector('.modal-container');
-    const cancelBtn = <HTMLButtonElement>document.querySelector('.cancel-delete');
-    const yesDeleteBtn = <HTMLButtonElement>document.querySelector('.yes-delete');
-    const editBtn = replyElement.querySelectorAll('.edit-btn') as NodeListOf<HTMLButtonElement>;
-    const updateBtn = <HTMLButtonElement>replyElement.querySelector('.update-button');
-    const replyContent = <HTMLParagraphElement>replyElement.querySelector('.user-reply-content');
-    const addReply = <HTMLTextAreaElement>replyElement.querySelector('.add-reply');
-    const replyForm = <HTMLFormElement>replyElement.querySelector('.reply-form');
-    const upvoteBtn = <HTMLButtonElement>replyElement.querySelector('.upvote-btn');
-    const downvoteBtn = <HTMLButtonElement>replyElement.querySelector('.downvote-btn');
-    const voteScoreSpan = <HTMLSpanElement>replyElement.querySelector('.vote-score');
-    const updateContentContainer = <HTMLDivElement>replyElement.querySelector('.update-content-container');
-    const replyBtn = replyElement.querySelectorAll('.reply-button') as NodeListOf<HTMLButtonElement>;
-
-    replyForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      replySubmission(addReply, commentId, replyLength, reply.user.username);
-      replyForm.classList.add('close');
-    });
-
-    if (editBtn) {
-      editBtn.forEach((editButton) => {
-        handleUpdatingReply(
-          editButton,
-          updateBtn,
-          updateContentContainer,
-          replyContent,
-          replyElement,
-          commentId,
-          reply.id,
-        );
-      });
-    }
-
-    if (cancelBtn && deleteBtn && yesDeleteBtn && overlay && modalContainer) {
-      deleteBtn.forEach((deleteButton) => {
-        handleDeleteReply(
-          cancelBtn,
-          deleteButton,
-          yesDeleteBtn,
-          overlay,
-          modalContainer,
-          commentId,
-          reply.id,
-        );
-      });
-    }
-
-    if (replyBtn) {
-      replyBtn.forEach((replyButton) => {
-        handleReplyButton(replyButton, replyForm, addReply);
-      });
-    }
-
-    handleUpdateReplyScore(upvoteBtn, downvoteBtn, voteScoreSpan, commentId, reply.id);
-
-    userAvatarContainer.appendChild(userAvatar);
-    replyLists.appendChild(replyElement);
+  replyForm?.addEventListener('submit', (event) => {
+    event.preventDefault();
+    replySubmission(addReply, commentId, replyLength, user.username);
+    replyForm.classList.add('close');
   });
 
-  return replyLists;
+  if (editBtn) {
+    editBtn.forEach((editButton) => {
+      handleUpdatingReply(
+        editButton,
+        updateBtn,
+        updateContentContainer,
+        replyContent,
+        replyElement,
+        commentId,
+        id,
+      );
+    });
+  }
+
+  if (deleteBtn) {
+    deleteBtn.forEach((deleteButton) => {
+      handleDeleteReply(deleteButton, commentId, id);
+    });
+  }
+
+  if (replyBtn) {
+    replyBtn.forEach((replyButton) => {
+      handleReplyButton(replyButton, replyForm, addReply);
+    });
+  }
+
+  handleUpdateReplyScore(upvoteBtn, downvoteBtn, voteScoreSpan, commentId, id);
+
+  userAvatarContainer.appendChild(userAvatar);
+  return replyElement;
 };
 
 export default ReplyCard;
